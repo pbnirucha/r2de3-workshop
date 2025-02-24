e7a4413dc090918b568ee44361d2343b42d0c6db
Nirucha Prasrisilsakul <nirucha.psk@gmail.com>
2025-02-24 15:25:36 +0000

    feat: add DummyOperator and dependency

--- dags/ws4_exercise3.py
+++ dags/ws4_exercise3.py
@@ -14,9 +14,16 @@ with DAG(
     # ใน exercise นี้จะได้รู้จักการเขียน task ใน pipeline ขั้นตอนเยอะขึ้น
     # ใช้ DummyOperator เป็น task จำลอง
     
-    t0 = DummyOperator(task_id="task_0")
-    t4 = DummyOperator(task_id="task_4")
-
-    t0 >> t4
+    # t0 = DummyOperator(task_id="task_0")
+    # t1 = DummyOperator(task_id="task_1")
+    # t2 = DummyOperator(task_id="task_2")
+    # t3 = DummyOperator(task_id="task_3")
+    # t4 = DummyOperator(task_id="task_4")
+    # t5 = DummyOperator(task_id="task_5")
+    # t6 = DummyOperator(task_id="task_6")
+   
+    t = [ DummyOperator(task_id= f"task_{i}") for i in range(7) ]
     
     # TODO: สร้าง DummyOperator เพื่อสร้าง dependency ที่ซับซ้อน ตามรูป
+    [t[0], t[1], t[2]] >> t[4]
+    [t[3], t[4], t[5]] >> t[6]
